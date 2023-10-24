50173276b83cc5fae739356ea10d6b9b470b3135
WENDY <wendy.munoz@bqm.com.pe>
2023-10-23 23:20:33 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html><html lang="en">
+
+<?PHP include("shorts/head.html"); ?>
+
+<body>
+	<main class="main">
+		<div class="main-inner">
+
+			<?PHP include("shorts/nav.html"); ?>
+
+			<div class="section-bgc intro">
+				<div class="intro-item intro-item-type-1" style="background-image: url('assets/img/intro-img12.jpg');">
+					<div class="container">
+						<div class="row">
+							<div class="col">
+								<div class="intro-content" style="--margin-left: 4rem;">
+									<div class="section-heading shm-none">
+										<h1>Productos ortopédicos de alta calidad</h1>
+										<p class="section-desc">Somos tu puerta de entrada a un mundo de productos ortopédicos de calidad y equipos de protección personal</p>
+									</div>
+									<div class="btn-group intro-btns">
+										<a href="" class="btn btn-border btn-with-icon btn-small ripple">
+											<span>Pedir información <i class="material-icons material-icons-outlined">whatsapp</i></span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<section class="section">
+				<div class="container">
+					<div class="row items">
+						<div class="col-12">
+							<div class="section-heading heading-center">
+								<h2>Nuestros Productos</h2>
+							</div> 
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 col-6 item">
+							<!-- Begin brands item -->
+							<div class="brands-item item-style">
+								<img data-src="assets/img/products/producto1.jpeg" class="lazy" alt="">
+							</div><!-- End brands item -->
+							<p class="text-center">Nombre del Producto - s/.50.00</p>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 col-6 item">
+							<!-- Begin brands item -->
+							<div class="brands-item item-style">
+								<img data-src="assets/img/products/producto2.jpeg" class="lazy" alt="">
+							</div><!-- End brands item -->
+							<p class="text-center">Nombre del Producto - s/.50.00</p>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 col-6 item">
+							<!-- Begin brands item -->
+							<div class="brands-item item-style">
+								<img data-src="assets/img/products/producto3.jpeg" class="lazy" alt="">
+							</div><!-- End brands item -->
+							<p class="text-center">Nombre del Producto - s/.50.00</p>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 col-6 item">
+							<!-- Begin brands item -->
+							<div class="brands-item item-style">
+								<img data-src="assets/img/products/producto4.jpeg" class="lazy" alt="">
+							</div><!-- End brands item -->
+							<p class="text-center">Nombre del Producto - s/.50.00</p>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 col-6 item">
+							<!-- Begin brands item -->
+							<div class="brands-item item-style">
+								<img data-src="assets/img/products/producto5.jpeg" class="lazy" alt="">
+							</div><!-- End brands item -->
+							<p class="text-center">Nombre del Producto - s/.50.00</p>
+						</div>
+						<div class="col-lg-4 col-md-4 col-sm-4 col-6 item">
+							<!-- Begin brands item -->
+							<div class="brands-item item-style">
+								<img data-src="assets/img/products/producto6.jpeg" class="lazy" alt="">
+							</div><!-- End brands item -->
+							<p class="text-center">Nombre del Producto - s/.50.00</p>
+						</div>
+					</div>
+					<div class="section-footer col-12 section-footer-animate">
+						<div class="btn-group align-items-center justify-content-center">
+							<a href="#!" class="btn btn-with-icon btn-w240 ripple">
+								<span>Ir Catálogo</span>
+								<svg class="btn-icon-right" viewBox="0 0 13 9" width="13" height="9"><use xlink:href="assets/img/sprite.svg#arrow-right"></use></svg>
+							</a>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="section section-bgc">
+				<div class="container">
+					<div class="row litems">
+						<header class="col-12">
+							<div class="section-heading heading-center">
+								<!--div class="section-subheading">some reasons</div-->
+								<h2>¿Por qué nosotros?</h2>
+							</div> 
+						</header>
+						<div class="col-lg-4 col-md-6 col-12 litem">
+							<div class="ini">
+								<div class="ini-count">01</div>
+								<div class="ini-info">
+									<h3 class="ini-heading item-heading-large">Calidad Garantizada</h3>
+									<div class="ini-desc">
+										<p>Nuestros productos cumplirán con tus expectativas.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 col-12 litem">
+							<div class="ini">
+								<div class="ini-count">02</div>
+								<div class="ini-info">
+									<h3 class="ini-heading item-heading-large">Rapidez de envío</h3>
+									<div class="ini-desc">
+										<p>Envíos a todo el Perú.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 col-12 litem">
+							<div class="ini">
+								<div class="ini-count">03</div>
+								<div class="ini-info">
+									<h3 class="ini-heading item-heading-large">Asesoría por Expertos</h3>
+									<div class="ini-desc">
+										<p>Contamos con expertos que te ayudarán a elegir el producto ideal.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+
+		<?PHP include("shorts/foot.html"); ?>
+
+	</main><!-- End main -->
+
+	<script src="assets/libs/jquery/jquery.min.js"></script>
+	<script src="assets/libs/lozad/lozad.min.js"></script>
+	<script src="assets/libs/device/device.js"></script>
+	<script src="assets/libs/spincrement/jquery.spincrement.min.js"></script>
+	<script src="assets/libs/pristine/pristine.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+	<script src="assets/js/forms.js"></script>
+	
+</body></html>
